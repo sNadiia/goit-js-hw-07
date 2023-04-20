9d37eee9f6e987de13a1c1c63d13c7e91f68721e
Nadiia Samborska <dilova_3@ukr.net>
2023-04-20 18:16:27 +0300

    RenderGallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,15 +2,40 @@ import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 const galleryRef = document.querySelector(".gallery");
 
-const markup = galleryItems
-  .map(
-    (galleryItem) =>
-      `<li class="gallery__item"><img class="gallery__image" src="${galleryItem.preview}" alt="${galleryItem.description}"></li>`
-  )
-  .join("");
+const renderGallery = (imgs) =>
+  imgs.reduce(
+    (acc, { preview, original, description }) =>
+      acc +
+      `<li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
+      </a>
+    </li>`,
+    ""
+  );
 
-galleryRef.innerHTML = markup;
+const insertRenderGallery = (string) => {
+  galleryRef.insertAdjacentHTML("beforeend", string);
+};
 
+insertRenderGallery(renderGallery(galleryItems));
 
 console.log(galleryItems);
-// srcset="${galleryItem.original}"
+
+galleryRef.onclick = (e) => {
+  e.preventDefault();
+  if (e.target.nodeName === "IMG") {
+    basicLightbox
+      .create(
+        `<img width="1400" height="900" src="${e.target.dataset.source}">`
+      )
+      .show();
+  } else {
+    return;
+  }
+};
